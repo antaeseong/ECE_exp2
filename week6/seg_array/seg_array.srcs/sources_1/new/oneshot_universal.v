db7250cbbb6ff53f2db6efe99e296661a939e06c
antaeseong <ast0417@naver.com>
2022-10-10 23:51:08 +0900

    Create oneshot_universal.v

---
+++ week6/seg_array/seg_array.srcs/sources_1/new/oneshot_universal.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2022/10/08 10:39:07
+// Design Name: 
+// Module Name: oneshot_universal
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module oneshot_universal(
+
+    );
+endmodule
