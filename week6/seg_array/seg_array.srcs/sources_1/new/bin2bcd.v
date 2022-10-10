107434d8d2b34a13303f2562ea98ea55733e4483
antaeseong <ast0417@naver.com>
2022-10-10 23:50:44 +0900

    Create bin2bcd.v

---
+++ week6/seg_array/seg_array.srcs/sources_1/new/bin2bcd.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2022/10/08 10:39:22
+// Design Name: 
+// Module Name: bin2bcd
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module bin2bcd(
+
+    );
+endmodule
